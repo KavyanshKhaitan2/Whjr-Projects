509b6a5eca84f3bb78d3ef831d474984460608b0
KavyanshKhaitan2 <73186427+KavyanshKhaitan2@users.noreply.github.com>
2021-05-14 10:11:23 +0530

    Add files via upload

---
+++ Project 77/script.js
@@ -0,0 +1,19 @@
+var first_name = "Kavyansh ";
+var last_name = "Khaitan";
+
+var no1 = 97;
+var no2 = 36;
+
+console.log("My first console project");
+console.log(first_name.concat(last_name));
+
+
+add = no1 + no2;
+subtract = no1 - no2;
+multiply = no1 * no2;
+divide = no1 / no2;
+
+console.log(add);
+console.log(subtract);
+console.log(multiply);
+console.log(divide);
