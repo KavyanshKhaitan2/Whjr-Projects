7935f13661d4d7012d620914b1a9a613e210a868
KavyanshKhaitan2 <73186427+KavyanshKhaitan2@users.noreply.github.com>
2022-01-21 20:05:37 +0530

    Add files via upload

---
+++ Project 114/script.js
@@ -0,0 +1,20 @@
+//////////////////////
+// Code By Kavyansh //
+//////////////////////
+
+function preload() { // Defines a function preload which is meant to load assets before the setup phase
+
+}
+
+function setup() { // Defines a function setup which creates the canvss and sets up everything for use
+  canvas = createCanvas(300, 300) // Creates the canvas
+  canvas.center(); // Moves the canvas to the center
+}
+
+function draw() { // Draws, this is a repeat loop which runs infinitely
+
+}
+
+function download_image() { // Onclick: Download Filtered Image button
+
+}
