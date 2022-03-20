5a199f808c895be0796df9966414395f83b0ce11
KavyanshKhaitan2 <73186427+KavyanshKhaitan2@users.noreply.github.com>
2022-03-20 09:42:09 +0530

    Add files via upload

---
+++ Project 120/config.js
@@ -0,0 +1,19 @@
+c=
+{
+  // All your config goes in here!
+  // Most of the configuration is formattable in HTML
+  cards: [
+    // Enter cards in a syntax similar to this one:
+    {
+      topic: "Topic 1",
+      details: [
+        // NOTE: Details support a max of 2 entries
+        ["header here", "description here"],
+        ["header2 here", "description2 here"],
+      ],
+      result: "",
+      imagePath: ""
+    },
+  ],
+  summary: ""
+}
