4716bb9ec780f76153b952517aab7b4463ccc215
KavyanshKhaitan2 <73186427+KavyanshKhaitan2@users.noreply.github.com>
2022-03-20 13:37:13 +0530

    Add files via upload

---
+++ Project 121/config.js
@@ -0,0 +1,95 @@
+c=
+{
+  // All your config goes in here!
+  // Most of the configuration is formattable in HTML
+  cards: [
+    // Enter cards in a syntax similar to this one:
+    {
+      topic: "Logitech Media Keyboard K200",
+      details: [
+        // NOTE: Details support a max of 2 entries
+        ["Google Lens", "Logitech K200 Keyboard (+5)"],
+        ["MobileNet", "Computer Keyboard, Keypad (+2)"],
+      ],
+      result: `Google Lens is more accurate.
+      <br>Even though MobileNet is usably accurate, Google Lens wins this point<br>
+      2 poins for MobileNet because it works even in offline mode even though Google Lens cant.<br>
+      <span style='font-size:x-small'>piramid :D</span>`,
+
+      imagePath: "images/card-0-logitech-media-keyboard-k-200.png",
+      imageHeight: "150px",
+    },
+
+    {
+      topic: "Logitech Media Mouse",
+      details: [
+        // NOTE: Details support a max of 2 entries
+        ["Google Lens", "M90 Logitech Mouse (+5)"],
+        ["MobileNet", "Mouse, Computer Mouse (+2)"],
+      ],
+      result: "Google Lens is more accurate.",
+      imagePath: "images/card-1-logitech-media-mouse.png",
+      imageHeight: "250px",
+    },
+
+    {
+      topic: "Water Bottle",
+      details: [
+        // NOTE: Details support a max of 2 entries
+        ["Google Lens", "Cylinder (+0)"],
+        ["MobileNet", "Water Bottle (+5)"],
+      ],
+      result: "MobileNet is more accurate. <span style='font-size:x-small'>*applies to specific items only.</span>",
+      imagePath: "images/card-2-water-bottle.png",
+      imageHeight: "29vh",
+    },
+
+    {
+      topic: "Dell 19.5 inch LED Computer Monitor E2016HV",
+      details: [
+        // NOTE: Details support a max of 2 entries
+        ["Google Lens", "Dell Monitor 18.5 inch (+5)"],
+        ["MobileNet", "Television System (+1)"],
+      ],
+      result: "Google Lens is more accurate even though theres a size mismatch.",
+      imagePath: "images/card-3-dell-19-5-inch-led-computer-monitor-e2016hv.png",
+      imageHeight: "29vh",
+    },
+
+    {
+      topic: "Wooden Door",
+      details: [
+        // NOTE: Details support a max of 2 entries
+        ["Google Lens", "Rectangular Door (+4)"],
+        ["MobileNet", "Sliding Door (+2)"],
+      ],
+      result: "Google Lens is more accurate.",
+      imagePath: "images/card-4-wooden-door.png",
+      imageHeight: "29vh",
+    },
+
+    {
+      topic: "8GB Strontium USB Pendrive",
+      details: [
+        // NOTE: Details support a max of 2 entries
+        ["Google Lens", "Strontium Pollex Series 8GB Flash Drive Set (+10)"],
+        ["MobileNet", "Digital Clock (-1)"],
+      ],
+      result: "Google Lens is just a bit too much accurate while MobileNet is acting dumb.",
+      imagePath: "images/card-5-strontium-8GB-pendrive.png",
+      imageHeight: "29vh",
+    },
+
+    {
+      topic: "Apple as in a Fruit",
+      details: [
+        // NOTE: Details support a max of 2 entries
+        ["Google Lens", "Red Apple (+5)"],
+        ["MobileNet", "Mouse, Computer Mouse (-1)"],
+      ],
+      result: "Google Lens is accurate while MobileNet is acting <i>very</i> dumb.",
+      imagePath: "images/card-6-apple.png",
+      imageHeight: "29vh",
+    },
+  ],
+}
---
+++ Project 121/main.js
@@ -0,0 +1,53 @@
+console.log("Initializing Case Study cards... (it may take a few seconds)")
+body = document.getElementsByTagName("body")[0]
+print = console.log
+
+try {
+  if (c != null) {
+    print("Config found...")
+  }
+}
+catch(err) {
+  location.reload()
+  alert(`ALERT!
+         Config not found and/or invalid config.
+         If you are the owner of this webpage,
+         please populate the ~/config.js file, initial
+         config can be found at ~/customize/index.html
+         -----------------------------------------------------
+         If you are a visitor, please wait a
+         few mimutes or contact the webpage owner`)
+}
+
+print("Looks like the config is loaded!")
+print({Config:c})
+cardnamelist = []
+for (let index = 0; index < c.cards.length; index++) {
+  const i = c.cards[index];
+  cardnamelist.push(i.topic)
+}
+print({Cards:c.cards})
+
+for (let i = 0; i < cardnamelist.length; i++) {
+  element = cardnamelist[i]
+  document.getElementById("shortcuts").innerHTML += "<li><a href='#card-"+i+"'>Here</a> for "+element+"</li>"
+  template = document.querySelector("template#card-template").innerHTML
+  template = template.replaceAll("{card-topic}", c.cards[i].topic)
+  template = template.replaceAll("{detail1 header}", c.cards[i].details[0][0])
+  template = template.replaceAll("{detail1 description}", c.cards[i].details[0][1])
+  template = template.replaceAll("{detail2 header}", c.cards[i].details[1][0])
+  template = template.replaceAll("{detail2 description}", c.cards[i].details[1][1])
+  template = template.replaceAll("{card-result}", c.cards[i].result)
+  template = template.replaceAll("{card-index}", i)
+  if(c.cards[i].imagePath) {
+    template = template.replaceAll("{card-image-alt}", c.cards[i].topic)
+    template = template.replaceAll("{card-image}", c.cards[i].imagePath)
+    template = template.replaceAll("{imageHeight}", c.cards[i].imageHeight)
+  } else {
+    template = template.replaceAll("{card-image-alt}", "")
+    template = template.replaceAll("{card-image}", "")
+    template = template.replaceAll("{imageHeight}", "")
+  }
+  document.getElementById("cards").innerHTML += template;
+  print(body.querySelector("#card-"+i).querySelector(".card-image").style.height = c.cards[i].imageHeight)
+}
