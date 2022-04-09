d32ac7e7e06b0347b6cf0e3ea3cbaaba354e37ac
KavyanshKhaitan2 <73186427+KavyanshKhaitan2@users.noreply.github.com>
2022-04-09 20:00:01 +0530

    Add files via upload

---
+++ Project 123 font man/script.js
@@ -0,0 +1,18 @@
+function preload() {
+
+}
+
+function setup() {
+  canvas = createCanvas(500, 500)
+  canvas.center()
+  canvas.position(window.innerWidth - 600, "")
+
+  video = createCapture(VIDEO)
+  video.size(500, 500)
+  video.center()
+  video.position(100, "")
+}
+
+function draw() {
+
+}
